4ccd4748a55dd3b98c21f2f1459eae929a3ca6db
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-06-15 19:10:42 +0530

    update-commit

--- Day_16/script.js
+++ Day_16/script.js
@@ -28,10 +28,14 @@ console.log(sum);
 
 //foreach does not return anything
 
+
+
+
+
 // ************************************* MAP ***********************************************
 
 //print the double of array and square===== map 
-// map can return also 
+// map can return also (to transform the array values)
 var arr=[10,20,30,40];
 
 var arr2= arr.map(function(elem){
@@ -43,3 +47,51 @@ var arr3= arr.map(function(elem){
     return  elem*elem;
 })
 console.log(arr3);
+
+
+// we can perform any operation on array on the passed elements and return the value as per our requirement
+var users = ['riddh', 'siddharth', 'priyanshu'];
+var newusers= users.map(function(elem){
+    return elem.length;
+})
+console.log(newusers);
+
+//print even and odd- if the value is even then return elem and if the value is odd then return +1
+var arr=[10,11,12,13,14];
+var arr2= arr.map(function(elem){
+    if(elem%2==0)
+    {
+        return elem;
+    }
+    else
+    {
+        return elem+1;
+    }
+})
+console.log(arr2);
+
+//it returns undefined if we do not return anything in the map function
+// map- transform ,length same rahegi , har ele add hoga
+
+
+
+// ************************************** FILTER ***********************************************
+
+// to filter the values from the current array and return a new array as per the condition provided in the callback function
+// it return [] if no value is found as per the condition
+//select , length change ho skti hai , sirf match ele add honge
+
+var arr=[10,-11,12,13,-14];
+var arr2=arr.filter(function(elem){
+    //in this with return a condition is passed
+ return elem>0;// it will return only the positive values in the new array
+})
+console.log(arr2);
+
+//return elem.includes('a')// it will return the values which have a in it
+var users = ['riddh', 'siddharth', 'priyanshu'];
+var newusers= users.filter(function(elem){
+    return elem.includes('a');
+})
+console.log(newusers);
+
