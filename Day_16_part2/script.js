827f5908b8069639ac2178bdb58a0729f404fa63
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-06-18 12:57:02 +0530

    update-commit

--- Day_16_part2/script.js
+++ Day_16_part2/script.js
@@ -100,3 +100,77 @@ Object.seal(user1);//it will not allow to add or delete any property from object
 //we can update value in seal but we cannot add or delete any property
 
 Object.freeze(user1);//it will not allow to add, delete or update any property from object  
+
+
+
+//destructuring of array
+var arr=[11,22,33,44]
+var [a,b,c,d]=arr;
+console.log(a,b,c,d);
+
+var arr=['aman','vaibhav','krishan','ritik'];
+var [x,y,...z]=arr;//rest operator
+//if we use[...] in left of = then it is rest operator and it will store the remaining values in an array, rest element must be last element in array
+//whereas if we use [...] in right of = then it is spread operator and it will spread the values of array
+console.log(x,y,z);
+
+
+//destructuring of object
+var obj={
+    name: 'aman',
+    age:20,
+    batch:'cohort 3'
+}
+var {name,age,batch}=obj//we have to use same variable name as key name in object while destructuring
+console.log(name,age,batch);
+
+//or
+
+var{name,...b}=obj
+console.log(b);
+console.log(name,b);
+
+//copyinh an array using spread operator
+var arr=[1,2,3,4,5];
+var arr2=[...arr];  
+arr2.push(6);
+console.log(arr);
+console.log(arr2);
+
+//copying an object using spread operator
+var obj={
+    name:'krishan',
+    age:22, 
+}
+var obj2={...obj};
+obj2.city='Delhi';
+console.log(obj);
+console.log(obj2);
+//this is a shallow copy it is not very perfect
+var obj={
+    name:'krishan',
+    age:22,
+    marks:55,
+    college:{
+        name:'abc',
+        city:'Delhi'
+    }
+}
+var obj2= {...obj};
+obj2.college.city='Mumbai';
+console.log(obj);
+console.log(obj2);
+//this is a shallow copy it is not very perfect because it will change the value of nested object in both objects
+
+
+//deep copy - it will create a new object with new reference and it will not change the value of nested object in both objects
+// json.stringify()//it will convert object to string
+var a=10
+var b=JSON.stringify(a);
+console.log(typeof a);
+console.log(b);
+console.log(typeof b);
+// json.parse()//it will convert string to object
+var c=JSON.parse(b);
+console.log(c);
+console.log(typeof c);
